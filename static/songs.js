89f40c0c11bcf0cbf2cfbe0526caeb13f5d62f73
Mathew Wright <m4_wright@hotmail.com>
2017-07-23 18:29:32 -0400

    add an order to the artists

--- static/songs.js
+++ static/songs.js
@@ -1,24 +1,37 @@
 var songApp = angular.module("songApp", []);
 
+var songs;
+var song_names;
 
 function getSongs($scope) {
-    $.post("http://192.168.0.134:8080/get_songs", {artist: document.title}, (songs, error) => {
+    $.post("http://192.168.0.134:8080/get_songs", {artist: document.title}, (song_list, error) => {
         $scope.$apply(() => {
-            $scope.songs = songs;
+            $scope.songs = song_list;
+            songs = song_list;
             $scope.song_names = Object.keys(songs);
+            song_names = $scope.song_names;
             $scope.current_song_path = "http://192.168.0.134:8080" + $scope.songs[$scope.song_names[0]].substr(1);
         });
     });
 }
 
-
+function nextSong(song_path) {
+    for (let i = 0; i < song_names.length; i++) {
+        if (song_names[i] === song_path) {
+            return (i + 1) % song_names.length;
+        }
+    }
+    return 0;
+}
 
 
 songApp.controller("songCtrl", function($scope) {
     getSongs($scope);
     $scope.play_song = function(song) {
-        console.log("new song name: " + song);
         $scope.current_song_path = encodeURI("http://192.168.0.134:8080" + $scope.songs[song].substr(1));
-        console.log("new song path: " + $scope.current_song_path);
     };
+    var audio = document.getElementById("audio");
+    audio.addEventListener('ended', function() {
+        $scope.play_song(nextSong(audio.src));
+    });
 });
