fc400cea07c7889de0a42c9d1f8c8a2eefd0cd2b
Mathew Wright <m4_wright@hotmail.com>
2017-07-23 16:52:45 -0400

    add an order to the artists

--- static/songs.js
+++ static/songs.js
@@ -5,7 +5,6 @@ function getSongs($scope) {
     $.post("http://192.168.0.134:8080/get_songs", {artist: document.title}, (songs, error) => {
         $scope.$apply(() => 
             $scope.songs = songs);
-            getSongNames($scope);
     });
 }
 
