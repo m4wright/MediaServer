4907b3f6ce56071bb06959451e108228f2a532be
Mathew Wright <m4_wright@hotmail.com>
2017-07-23 15:13:59 -0400

    fixed bug

--- static/songs.js
+++ static/songs.js
@@ -1,7 +1,5 @@
 var songApp = angular.module("songApp", []);
 
 songApp.controller("songCtrl", function($scope) {
-    $scope.$apply(() => {
         $scope.songs = ['song 1', 'song 2'];
-    });
 });
