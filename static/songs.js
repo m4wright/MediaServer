f014d8dc675054ad6fbb4c0c3450dc6a7e27503e
Mathew Wright <m4_wright@hotmail.com>
2017-07-23 18:06:54 -0400

    add an order to the artists

--- static/songs.js
+++ static/songs.js
@@ -6,7 +6,7 @@ function getSongs($scope) {
         $scope.$apply(() => {
             $scope.songs = songs;
             $scope.song_names = Object.keys(songs);
-            $scope.current_song_path = "http://192.168.0.134:8080" + $scope.songs[$scope.song_names[0]].substr(1);
+            $scope.current_song_path = $scope.songs[$scope.song_names[0]];
         });
     });
 }
@@ -18,7 +18,7 @@ songApp.controller("songCtrl", function($scope) {
     getSongs($scope);
     $scope.play_song = function(song) {
         console.log("new song name: " + song);
-        $scope.current_song_path = "http://192.168.0.134:8080" + $scope.songs[song].substr(1);
+        $scope.current_song_path = $scope.songs[song];
         console.log("new song path: " + $scope.current_song_path);
     };
 });
