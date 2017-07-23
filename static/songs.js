4cca3b46eed95233b1809ed8db5f3e4ab2f8e144
Mathew Wright <m4_wright@hotmail.com>
2017-07-23 17:31:26 -0400

    add an order to the artists

--- static/songs.js
+++ static/songs.js
@@ -3,15 +3,14 @@ var songApp = angular.module("songApp", []);
 
 function getSongs($scope) {
     $.post("http://192.168.0.134:8080/get_songs", {artist: document.title}, (songs, error) => {
-        $scope.$apply(() => 
-            $scope.songs = songs);
-            // $scope.song_names = Object.keys(songs);
-            // console.log("keys: " + JSON.stringify($scope.song_names));
+        $scope.$apply(() => {
+            $scope.songs = songs;
+            $scope.song_names = Object.keys(songs);
+        });
     });
 }
 
 
 songApp.controller("songCtrl", function($scope) {
     getSongs($scope);
-    $scope.song_names = ['Never Thought That This Would Happen'];
 });
