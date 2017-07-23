1c9de58d9c27898dde206fabec24740521f77478
Mathew Wright <m4_wright@hotmail.com>
2017-07-23 18:07:57 -0400

    add an order to the artists

--- static/songs.js
+++ static/songs.js
@@ -2,7 +2,7 @@ var songApp = angular.module("songApp", []);
 
 
 function getSongs($scope) {
-    $.post("http://192.168.0.134:8080/get_songs", {artist: document.title}, (songs, error) => {
+    $.post("../get_songs", {artist: document.title}, (songs, error) => {
         $scope.$apply(() => {
             $scope.songs = songs;
             $scope.song_names = Object.keys(songs);
