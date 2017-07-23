a3e3e78c283aa95fa2c9354ff647be243ceff6fc
Mathew Wright <m4_wright@hotmail.com>
2017-07-23 15:10:29 -0400

    fixed bug

--- static/songs.js
+++ static/songs.js
@@ -1,6 +1,8 @@
 var songApp = angular.module("songApp", []);
 
 songApp.controller("songCtrl", function($scope) {
-    $scope.artist = "Mathew";
-    $scope.songs = ['song 1', 'song 2'];
+    $scope.$apply(() => {
+        $scope.artist = "Mathew";
+        $scope.songs = ['song 1', 'song 2'];
+    });
 });
