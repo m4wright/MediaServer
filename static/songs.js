bceb7e5f7a9494c6f0dfbf3f54f05f5115aadcf3
Mathew Wright <m4_wright@hotmail.com>
2017-07-23 19:42:56 -0400

    add an order to the artists

--- static/songs.js
+++ static/songs.js
@@ -3,6 +3,8 @@ var songApp = angular.module("songApp", []);
 var songs;
 var song_names;
 
+var url = "http://192.168.0.134:8080";
+
 function getSongs($scope) {
     $.post("http://192.168.0.134:8080/get_songs", {artist: document.title}, (song_list, error) => {
         $scope.$apply(() => {
@@ -10,29 +12,28 @@ function getSongs($scope) {
             songs = song_list;
             $scope.song_names = Object.keys(songs);
             song_names = $scope.song_names;
-            $scope.current_song_path = "http://192.168.0.134:8080" + $scope.songs[$scope.song_names[0]].substr(1);
+            $scope.current_song_path = url + $scope.songs[$scope.song_names[0]].substr(1);
+            $scope.current_song = $scope.song_names[0];
         });
     });
 }
 
 function nextSong(song_path) {
     song_path = decodeURI(song_path);
-    console.log('path: ' + song_path);
-    console.log("other: " + songs[song_names[0]]);
     for (let i = 0; i < song_names.length; i++) {
         if (song_path.indexOf(songs[song_names[i]].substr(1)) >= 0) {
-            console.log("matched. Next: " + songs[song_names[(i + 1) % song_names.length]]);
             return song_names[(i + 1) % song_names.length];
         }
     }
-    return songs[song_names[0]];
+    return song_names[0];
 }
 
 
 songApp.controller("songCtrl", function($scope) {
     getSongs($scope);
     $scope.play_song = function(song) {
-        $scope.current_song_path = encodeURI("http://192.168.0.134:8080" + $scope.songs[song].substr(1));
+        $scope.current_song = song;
+        $scope.current_song_path = encodeURI(url + $scope.songs[song].substr(1));
         return $scope.current_song_path;
     };
     document.getElementById("audio").addEventListener('ended', function() {
