1b536fa21244d140409d0d2f574a99fa70324f08
Mathew Wright <m4_wright@hotmail.com>
2017-07-23 15:35:51 -0400

    fixed bug

--- static/songs.js
+++ static/songs.js
@@ -5,6 +5,7 @@ function getSongs($scope) {
     $.post("../get_songs", {artist: document.title}, (songs, error) => {
         $scope.$apply(() => 
             $scope.songs = songs);
+            console.log($scope.songs);
     });
 }
 
